328a3d663a5ea8f6810e218f77772041e34f5556
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2021-04-25 16:28:29 -0600

    Profile styles

--- user/forms.py
+++ user/forms.py
@@ -18,4 +18,4 @@ class EditUserForm(forms.ModelForm):
 class EditProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image']
+        fields = ['image', 'background']
--- user/models.py
+++ user/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True)
+    background = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.user.username
@@ -12,6 +13,14 @@ class Profile(models.Model):
     def imageUrl(self):
         try:
             img = self.image.url
+        except:
+            img = ''
+        return img
+
+    @property
+    def backgroundUrl(self):
+        try:
+            img = self.background.url
         except:
             img = ''
         return img

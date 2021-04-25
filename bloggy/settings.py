b546ebcd4678fad0f611888a49503867486eb68f
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2021-04-25 17:17:25 -0600

    Remove admin site

--- bloggy/settings.py
+++ bloggy/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -150,3 +149,5 @@ LOGIN_REDIRECT_URL = 'index'
 SESSION_COOKIE_SECURE = True
 
 CSRF_COOKIE_SECURE = True
+
+ADMINS = [os.environ.get('ADMINS')]

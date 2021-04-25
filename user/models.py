115ea779f6e5d292d8e8242223b39f5588a14e79
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2021-04-25 16:04:43 -0600

    Profile model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    @property
+    def imageUrl(self):
+        try:
+            img = self.user.url
+        except:
+            img = ''
+        return img

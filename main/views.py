caf0c8f66439f6e33a3d6933af4067f6ef5d2076
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2021-04-25 18:23:35 -0600

    Posts and gallery

--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
+    posts = Post.objects.all().order_by('-created')
     context = {
         'title': 'Home Page',
+        'posts': posts,
     }
     return render(request, 'main/index.html', context)

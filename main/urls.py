032c0fc3745813bda7f29ddec4736f5c2b7a1846
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2021-05-04 14:58:04 -0600

    Post create + detail views

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('posts/create/', views.PostCreateView.as_view(), name='create-post'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, DetailView
 from .models import Post
 
 def index(request):
@@ -8,3 +10,14 @@ def index(request):
         'posts': posts,
     }
     return render(request, 'main/index.html', context)
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['image', 'caption']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class PostDetailView(DetailView):
+    model = Post
